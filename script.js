d611cc686fb0e29c269d2e962603397558d8ba61
Justin Dong <justindong@justins-mbp.attlocal.net>
2023-05-02 14:55:55 -0400

    added generatePassword function

--- script.js
+++ script.js
@@ -1,6 +1,10 @@
 // Assignment Code
 var generateBtn = document.querySelector("#generate");
 
+function generatePassword() {
+  // YOUR CODE HERE
+}
+
 // Write password to the #password input
 function writePassword() {
   var password = generatePassword();
