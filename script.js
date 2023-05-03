7b572273626d0f825cae48589df989395cc8cf24
Justin <dong135790@gmail.com>
2023-05-03 19:41:12 -0400

    created object that will be used to generate password. added prompt for a limited character set.

--- script.js
+++ script.js
@@ -2,14 +2,22 @@
 var generateBtn = document.querySelector("#generate");
 
 function generatePassword() {
-  var lowercase = "abcdefghijklmnopqrstuvwxyz";
-  var uppercase = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+  var generatedList = [
+    {lowercase:  "abcdefghijklmnopqrstuvwxyz"},
+    {uppercase: "ABCDEFGHIJKLMNOPQRSTUVWXYZ"},
+    {numeric: "1234567890"},
+    {specialCharacter: "`!@#$%^&*()-=[];',./_+{}:<>?"},
+  ];
 
-  var result = prompt("What is your name?");
-  var random = Math.floor(Math.random() * lowercase.length);
-  console.log("RANDOM", random, lowercase[random]);
+  var result = prompt("Please enter a value (Between 8 and 128 characters).");
+  if (result.length < 8 || result.length > 128) {
+    console.log("Hello");
+    alert("Please enter a value between 8 and 128 characters");
+  };
+  // var random = Math.floor(Math.random() * generatedList.length);
+  // console.log("RANDOM", random, generatedList[random]);
 
-  return "";
+  // return "";
 }
 
 // Write password to the #password input
